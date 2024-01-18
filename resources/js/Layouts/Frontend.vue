f8ff931b4a09fc1e1c53b7e22c5b86b4861575b9
regisatl <regisattolou21@outlook.fr>
2024-01-18 14:33:18 +0100

    Creation et ajout de code dans les fichier Header, Frontend et Welcome

---
+++ resources/js/Layouts/Frontend.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Header from "@/Components/Frontend/Header.vue";
+</script>
+
+<template>
+    <div class="bg-slate-200 dark:bg-slate-900">
+        <!----Header--->
+        <Header />
+        <!----Main--->
+        <main class="min-h-screen">
+             <slot />
+        </main>
+        <!----Footer--->
+    </div>
+</template>
+
+<style>
+</style>
