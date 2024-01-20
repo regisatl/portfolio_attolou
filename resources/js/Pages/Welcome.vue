6c734a911c6f29a6dca82c48e7f54b00f8ef90a5
regisatl <regisattolou21@outlook.fr>
2024-01-20 14:29:45 +0100

    Ajout de commentaire dans le welcome vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,12 +1,20 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
-import Frontend from "@/Layouts/Frontend.vue";
+import FrontendLayout from "@/Layouts/Frontend.vue";
 
 </script>
 
 <template>
     <Head title="Welcome to portfolio Attolou" />
-    <Frontend />
+    <FrontendLayout>
+        <!---Hero primary-->
+        <!---Promot tail-100-->
+        <!---About secondary-->
+        <!---Skills  tail-100-->
+        <!---Portfolio primary-->
+        <!---Services secondary-->
+        <!---Contact primary-->
+    </FrontendLayout>
 </template>
 
 <style></style>
