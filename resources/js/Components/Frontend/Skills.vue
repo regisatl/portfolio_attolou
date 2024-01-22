6422a16bff44e4ddc558b18215c6ec2fadbd8ca9
regisatl <regisattolou21@outlook.fr>
2024-01-22 13:08:50 +0100

    Ajout des skills effectué

--- resources/js/Components/Frontend/Skills.vue
+++ resources/js/Components/Frontend/Skills.vue
@@ -5,13 +5,14 @@ defineProps({
 </script>
 
 <template>
-    <section class="bg-amber-50 dark:bg-slate-800">
+    <section class="dark:bg-slate-800 py-12">
         <div class="container mx-auto">
+            <h2 class="text-center lg:text-3xl font-bold text-slate-800 dark:text-gray-100 mb-5">My Skills</h2>
             <div class="grid grid-cols-8 md:grid-flow-col">
-                <div class="flex items-center justify-center" v-for="skill in skills" :key="skill.id">
-                    <img :src="skill.image" :alt="skill.name" class="lg:h-12"/>
+                <div class="flex items-center justify-center" v-for="skill in skills.data" :key="skill.id">
+                    <img :src="skill.image" :alt="skill.name" class="lg:h-12 rounded-full"/>
                     <div class="ml-4">
-                        <h3 class="text-xl font-bold">{{ skill.name }}</h3>
+                        <h3 class="text-sm font-bold">{{ skill.name }}</h3>
                     </div>
                 </div>
             </div>
